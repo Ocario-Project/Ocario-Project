2ecefac648927952a92970bf89c796746c565fad
hoaflee <blockcrazy01@gmail.com>
2023-02-13 17:32:01 +0700

    commit

---
+++ contracts/zero-ex/contracts/src/vendor/IOcarioPool.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+  Copyright 2023 ZeroEx Intl.
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+pragma solidity ^0.6.12;
+
+interface IOcarioPool {
+/// @notice Swap token0 -> token1, or vice versa
+  /// @dev This method's caller receives a callback in the form of ISwapCallback#swapCallback
+  /// @dev swaps will execute up to limitSqrtP or swapQty is fully used
+  /// @param recipient The address to receive the swap output
+  /// @param swapQty The swap quantity, which implicitly configures the swap as exact input (>0), or exact output (<0)
+  /// @param isToken0 Whether the swapQty is specified in token0 (true) or token1 (false)
+  /// @param limitSqrtP the limit of sqrt price after swapping
+  /// could be MAX_SQRT_RATIO-1 when swapping 1 -> 0 and MIN_SQRT_RATIO+1 when swapping 0 -> 1 for no limit swap
+  /// @param data Any data to be passed through to the callback
+  /// @return qty0 Exact token0 qty sent to recipient if < 0. Minimally received quantity if > 0.
+  /// @return qty1 Exact token1 qty sent to recipient if < 0. Minimally received quantity if > 0.
+  function swap(
+    address recipient,
+    int256 swapQty,
+    bool isToken0,
+    uint160 limitSqrtP,
+    bytes calldata data
+  ) external returns (int256 qty0, int256 qty1);
+}
